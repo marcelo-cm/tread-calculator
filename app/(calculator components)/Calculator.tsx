47a202de028cd96d6909240e8c06d030d5fbd472
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-02-25 13:03:39 -0500

    full functionality. unstyled & unpolished.

--- app/(calculator components)/Calculator.tsx
+++ app/(calculator components)/Calculator.tsx
@@ -12,6 +12,7 @@ import { mathJSeval } from "../lib/calculator/mathJSeval";
 
 const Calculator = ({ dark }: { dark: boolean }) => {
   const [currentEquation, setCurrentEquation] = useState<string>("");
+  const [prevEquation, setPrevEquation] = useState<string>("");
   // You can encode the current equation in the URL so you can share to others
 
   function addElement(value: string | null) {
@@ -20,6 +21,11 @@ const Calculator = ({ dark }: { dark: boolean }) => {
 
   function deleteLast() {
     setCurrentEquation((prev) => prev.slice(0, -1));
+  }
+
+  function clearAll() {
+    setCurrentEquation("");
+    setPrevEquation("");
   }
 
   async function solveEquation() {
@@ -32,6 +38,7 @@ const Calculator = ({ dark }: { dark: boolean }) => {
         return;
       }
 
+      setPrevEquation(currentEquation);
       setCurrentEquation(result);
     } catch (e) {
       console.log(e);
@@ -39,79 +46,84 @@ const Calculator = ({ dark }: { dark: boolean }) => {
   }
 
   return (
-    <div className="w-full border p-4">
+    <div className="w-full max-w-[650px] border p-4">
+      {prevEquation ? <div>{prevEquation}</div> : null}
       <div className="border p-4 text-white">{currentEquation}</div>
       <div className="border flex flex-row gap-8">
-        <CalculatorButton handleItemClick={addElement} value={"0"}>
-          0
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"1"}>
-          1
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"2"}>
-          2
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"3"}>
-          3
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"4"}>
-          4
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"5"}>
-          5
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"6"}>
-          6
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"7"}>
-          7
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"8"}>
-          8
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"9"}>
-          9
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"0"}>
-          0
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"*"}>
-          <Cross1Icon />
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"+"}>
-          <PlusIcon />
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"-"}>
-          <DashIcon />
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"/"}>
-          <div className="w-[15px] h-[15px] flex justify-center items-center">
-            ÷
+        <div className="grid grid-cols-3 gap-4">
+          <CalculatorButton handleItemClick={addElement} value={"1"}>
+            1
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"2"}>
+            2
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"3"}>
+            3
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"4"}>
+            4
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"5"}>
+            5
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"6"}>
+            6
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"7"}>
+            7
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"8"}>
+            8
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"9"}>
+            9
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"0"}>
+            0
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"."}>
+            .
+          </CalculatorButton>
+        </div>
+        <div className="grid grid-cols-2 gap-8">
+          <div className="col-span-2">
+            <CalculatorButton handleItemClick={deleteLast}>
+              DEL
+            </CalculatorButton>
           </div>
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={"("}>
-          <div className="w-[15px] h-[15px] flex justify-center items-center">
-            (
-          </div>
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={addElement} value={")"}>
-          <div className="w-[15px] h-[15px] flex justify-center items-center">
-            )
-          </div>
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={deleteLast}>
-          <div className="w-[15px] h-[15px] flex justify-center items-center">
-            DEL
-          </div>
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={() => setCurrentEquation("")}>
-          <div className="w-[15px] h-[15px] flex justify-center items-center">
-            C
-          </div>
-        </CalculatorButton>
-        <CalculatorButton handleItemClick={solveEquation}>
-          <PauseIcon className="rotate-90" />
-        </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"*"}>
+            <Cross1Icon />
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"+"}>
+            <PlusIcon />
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"-"}>
+            <DashIcon />
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"/"}>
+            <div className="w-[15px] h-[15px] flex justify-center items-center">
+              ÷
+            </div>
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={"("}>
+            <div className="w-[15px] h-[15px] flex justify-center items-center">
+              (
+            </div>
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={addElement} value={")"}>
+            <div className="w-[15px] h-[15px] flex justify-center items-center">
+              )
+            </div>
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={clearAll}>
+            <div className="w-[15px] h-[15px] flex justify-center items-center">
+              C
+            </div>
+          </CalculatorButton>
+          <CalculatorButton handleItemClick={solveEquation}>
+            <PauseIcon className="rotate-90" />
+          </CalculatorButton>
+        </div>
       </div>
     </div>
   );
